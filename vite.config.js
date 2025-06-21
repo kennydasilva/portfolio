7b73926a9dfa5f1d969c49ff2d01732ab6b2a1be
Kenny Dasilva <kennydasilva542@gmail.com>
2025-06-21 13:27:03 +0200

    set up of tailwindcss configuration of the project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
+/**
+ * @fileoverview Vite configuration file for a React project with Tailwind CSS support.
+ * @author kenny
+ * @description Sets up Vite plugins for React and Tailwind CSS integration.
+ */
+/*
+* 
+*
+*/
+
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+
+// This is a Vite configuration file that sets up a React project with Tailwind CSS support.
+// It imports necessary plugins and exports a configuration object.
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
